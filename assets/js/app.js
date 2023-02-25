4f60b9b0b5fd1096972a6d39a76e10bd9b142354
Noemi Vadalà <noemi.vadala.nv@gmail.com>
2023-02-25 12:13:15 +0100

    Modifiche funzioni

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,50 +3,41 @@
 // 3 - add to cart
 // 4 - reset
 
-let divContainer = document.createElement('div');
-divContainer.className = "divContainer";
+function createElement(tag, nameClass, text) {
+    let element = document.createElement(tag);
+    element.className = nameClass;
+    element.innerHTML = text;
+    return element;
+}
 
-let counter = document.createElement('div');
-counter.className = "counter";
+let divContainer = createElement('div', 'divContainer', '')
+document.body.append(divContainer);
 
-let buttonIncrement = document.createElement('button');
-buttonIncrement.className = "buttonIncrement";
-buttonIncrement.innerHTML = "+";
+let divCart = createElement('div', 'divCart', '')
+let counter = createElement('div', 'counter', '')
+counter.id = 'counterDiv';
 
-let buttonDecrement = document.createElement('button');
-buttonDecrement.className = "buttonDecrement";
-buttonDecrement.innerHTML = "-";
-
-let inputNumber = document.createElement('input');
-inputNumber.className = "inputNumber";
+let buttonIncrement = createElement('button', 'buttonIncrement', '+')
+let buttonDecrement = createElement('button', 'buttonDecrement', '-')
+let inputNumber = createElement('input', 'inputNumber', '')
 inputNumber.type = "number";
 inputNumber.value = 0;
-
-let reset = document.createElement('button');
-reset.className = "reset";
-reset.innerHTML = "Cancella carrello";
-
-let add = document.createElement('button');
-add.className = "add";
-add.innerHTML = "Aggiungi al carrello";
-
+let add = createElement('button', 'add', 'Aggiungi al carrello')
+let reset = createElement('button', 'reset', 'Cancella carrello')
 // 1 - cart
-let cart = document.createElement('img');
-cart.className = "cart";
-cart.src = "img/cart.png";
-
-let quantity = document.createElement('img');
-quantity.className = "quantity cart";
-quantity.src = "img/quantity-cart.png";
-
-let valueQuantity = document.createElement('p');
-valueQuantity.className = "value-quantity";
+let cart = createElement('img', 'cart', '')
+cart.src = "assets/img/cart.png";
+let quantity = createElement('img', 'quantity', '')
+quantity.src = "assets/img/quantity-cart.png";
+let valueQuantity = createElement('p', 'value-quantity', '')
+valueQuantity.src = "assets/img/quantity-cart.png";
 valueQuantity.innerHTML = inputNumber.value;
 
-document.body.append(divContainer);
-divContainer.append(quantity, cart, valueQuantity, counter, add, reset);
-counter.append(buttonDecrement, inputNumber, buttonIncrement);
 
+divCart.append(cart, quantity, valueQuantity)
+divContainer.append(divCart, counter);
+counter.append(buttonDecrement, inputNumber, buttonIncrement, add, reset);
+/*
 // 2 - function
 buttonDecrement.onclick = function() {
     inputNumber.value--;
@@ -69,4 +60,24 @@ add.onclick = function() {
 reset.onclick = function() {
     inputNumber.value = 0;
     valueQuantity.innerHTML = inputNumber.value;
-}
+}*/
+
+let counterDiv = document.getElementById('counterDiv');
+
+counterDiv.addEventListener('click', function(e){
+    const target = e.target;
+
+    if(target == buttonIncrement) {
+        inputNumber.value++;
+    } else if (target == buttonDecrement) {
+        inputNumber.value--;
+        if (inputNumber.value <= 0){
+            inputNumber.value = 0;
+        }
+    } else if (target == add) {
+        valueQuantity.innerHTML = inputNumber.value;
+    } else if (target == reset) {
+        inputNumber.value = 0;
+        valueQuantity.innerHTML = inputNumber.value;
+    }
+})
